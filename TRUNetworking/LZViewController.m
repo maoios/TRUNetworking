ba492f2270c18792a8ebbc46c93cea1935ef341c
1246970327@qq.com <1246970327@qq.com>
2020-08-04 16:55:16 +0800

    init project

---
+++ TRUNetworking/LZViewController.m
@@ -0,0 +1,29 @@
+//
+//  LZViewController.m
+//  TRUNetworking
+//
+//  Created by 1246970327@qq.com on 08/04/2020.
+//  Copyright (c) 2020 1246970327@qq.com. All rights reserved.
+//
+
+#import "LZViewController.h"
+
+@interface LZViewController ()
+
+@end
+
+@implementation LZViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
